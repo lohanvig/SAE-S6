99890ba717184175f28808c5813995088973d065
antoine.tuffery <antoine.tuffery@iut-rodez.fr>
2025-05-22 11:12:04 +0200

    LocalDateTime deserialisation

--- legacy-software/src/main/java/sae/semestre/six/config/JacksonConfig.java
+++ legacy-software/src/main/java/sae/semestre/six/config/JacksonConfig.java
@@ -1,22 +1,17 @@
 package sae.semestre.six.config;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.databind.util.StdDateFormat;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import java.util.TimeZone;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 
 @Configuration
 public class JacksonConfig {
 
     @Bean
-    public ObjectMapper objectMapper() {
-        ObjectMapper mapper = new ObjectMapper();
-        mapper.setTimeZone(TimeZone.getTimeZone("Europe/Paris"));
-        mapper.setDateFormat(new StdDateFormat().withColonInTimeZone(true));
-        mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
-        return mapper;
+    public Jackson2ObjectMapperBuilder jacksonBuilder() {
+        Jackson2ObjectMapperBuilder builder = new Jackson2ObjectMapperBuilder();
+        builder.modules(new JavaTimeModule());
+        return builder;
     }
 }
